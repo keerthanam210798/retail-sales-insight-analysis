d0259d35b259553a0e9db91f6076a5d0de08e5e2
keerthanam210798 <keerthana.m210798@gmail.com>
2026-01-14 17:36:56 +0530

    Update sales_analysis.sql
    SQL queries

--- sql/sales_analysis.sql
+++ sql/sales_analysis.sql
@@ -1,1 +1,24 @@
+-- Total sales by category
+SELECT Category, SUM(Sales) AS total_sales
+FROM cleaned_superstore_sales
+GROUP BY Category
+ORDER BY total_sales DESC;
 
+-- Monthly sales trend
+SELECT order_month, SUM(Sales) AS monthly_sales
+FROM cleaned_superstore_sales
+GROUP BY order_month
+ORDER BY order_month;
+
+-- Sales by region
+SELECT Region, SUM(Sales) AS total_sales
+FROM cleaned_superstore_sales
+GROUP BY Region
+ORDER BY total_sales DESC;
+
+-- Top 10 products by sales
+SELECT Product_Name, SUM(Sales) AS total_sales
+FROM cleaned_superstore_sales
+GROUP BY Product_Name
+ORDER BY total_sales DESC
+LIMIT 10;
